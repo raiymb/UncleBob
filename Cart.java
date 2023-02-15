59fbee0dedec720ad22adffb0333805e24640e59
raiymb <123525700+raiymb@users.noreply.github.com>
2023-02-16 01:40:01 +0600

    Update Cart.java

--- Cart.java
+++ Cart.java
@@ -1,16 +1,38 @@
 import java.util.ArrayList;
 import java.util.List;
+
 public class Cart {
     private List<Film> cart;
 
     public Cart() {
         cart = new ArrayList<>();
     }
-    public void addToCart(Film film) {cart.add(film);}
 
-    public void removeFromCart(Film film) {cart.remove(film);}
+    public void addToCart(Film film) {
+        cart.add(film);
+    }
 
-    public List<Film> getCart() {return cart;}
+    public void removeFromCart(Film film) {
+        cart.remove(film);
+    }
 
+    public List<Film> getCart() {
+        return cart;
+    }
 
+    public double getTotalPrice() {
+        double totalPrice = 0;
+        for (Film film : cart) {
+            totalPrice += film.getPrice();
+        }
+        return totalPrice;
+    }
+
+    public void clearCart() {
+        cart.clear();
+    }
+
+    public boolean isCartEmpty() {
+        return cart.isEmpty();
+    }
 }
